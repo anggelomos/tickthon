bb0eebc7b2f8ddcd79353cb580a08afc5e0347dd
Angelo Mosquera <anggelomos@outlook.com>
2023-10-05 13:38:21 -0500

    Added py.typed

--- src/tickthon/_config.py
+++ src/tickthon/_config.py
@@ -1,8 +1,9 @@
 import os
 import json
+from typing import Dict
 
 CHECKINS_START_DATE = 20221231
 
 
-def get_ticktick_ids():
+def get_ticktick_ids() -> Dict[str, str]:
     return json.loads(os.getenv("TICKTICK_IDS", default="")) if os.getenv("TICKTICK_IDS") else None
